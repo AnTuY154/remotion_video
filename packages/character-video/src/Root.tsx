aba906f8f3203607009d6531074c5f8375f69755
Đỗ Trọng Anh Tuấn <55184774+AnTuY154@users.noreply.github.com>
2026-09-15 16:58:23 +0700

    feat(doi3d): register 3D architecture proof composition

--- packages/character-video/src/Root.tsx
+++ packages/character-video/src/Root.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import {Composition} from "remotion";
+import {Doi3DProof} from "./scenes/Doi3DProof";
 import {DoiYarnDemo} from "./scenes/DoiYarnDemo";
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Composition
-      id="DoiYarnDemo"
-      component={DoiYarnDemo}
-      durationInFrames={300}
-      fps={30}
-      width={1280}
-      height={720}
-    />
+    <>
+      <Composition
+        id="Doi3DProof"
+        component={Doi3DProof}
+        durationInFrames={240}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+      <Composition
+        id="DoiYarnDemo"
+        component={DoiYarnDemo}
+        durationInFrames={300}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+    </>
   );
 };
